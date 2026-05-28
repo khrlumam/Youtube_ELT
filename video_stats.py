ba7589f0cba0dc0790627156c0f30d6d1044d0f7
Khairul Umam <umam14719@gmail.com>
2026-05-28 14:26:08 +0700

    finish code to get statistics YT Video add update .gitignore

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
@@ -99,9 +100,14 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+def save_to_json(extracted_data):
+    file_path = f"./data/{CHANNEL_HANDLE}_data_{date.today()}.json"
 
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     playlistId = get_channel_playlistId()
     video_ids = get_video_ids(playlistId)
     videoData = extract_video_data(video_ids)
+    save_to_json(videoData)
